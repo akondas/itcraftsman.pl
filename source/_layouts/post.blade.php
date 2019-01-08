75d58dde67ced15101376c8c6eb00cc4f8feed9b
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2019-01-08 23:16:47 +0100

    Connect disqus for comments

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -47,4 +47,22 @@
             @endif
         </div>
     </nav>
+
+    <div class="comments mt-10">
+        <div id="disqus_thread"></div>
+        <script>
+            var disqus_config = function () {
+                this.page.url = "https://itcraftsman.pl{{ $page->getUrl() }}";
+                this.page.identifier = "{{ $page->getUrl() }}";
+            };
+            (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://itcraftsman.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+    </div>
 @endsection
